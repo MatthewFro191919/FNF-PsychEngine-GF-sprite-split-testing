77572e2c1f96daedcc75f21d3b0c548dd7851aff
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-21 12:20:59 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -6,6 +6,7 @@ import android.content.Context;
 
 import debug.FPSCounter;
 
+import flixel.system.FlxAssets;
 import flixel.graphics.FlxGraphic;
 import flixel.FlxGame;
 import flixel.FlxState;
@@ -46,9 +47,41 @@ import backend.Highscore;
 @:cppFileCode('#define GAMEMODE_AUTO')
 #end
 
+class InitState extends FlxState
+{
+    override function create():Void
+	{
+        super.create();
+
+		//Load Settings / Mods
+        Conductor.init();
+		CoolUtil.init();
+		Highscore.load();
+		#if discord_rpc
+		DiscordClient.initialize();
+		lime.app.Application.current.onExit.add((code:Int) -> DiscordClient.shutdown());
+        #end
+
+		FlxG.worldBounds.set(-10, -10, FlxG.width + 20, FlxG.height + 20);
+		FlxG.fixedTimestep = false;
+
+		FlxG.switchState(new funkin.Preloader());
+    }
+}
+
 // // // // // // // // //
 class Main extends Sprite
 {
+	final settings = {
+		width: 1280, 					// Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+		height: 720, 					// Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+		initialState: InitState,		// The FlxState the game starts with.
+		zoom: -1.0, 					// If -1, zoom is automatically calculated to fit the window dimensions.
+		framerate: 60, 					// How many frames per second the game should run at.
+		skipSplash: true, 				// Whether to skip the flixel splash screen that appears in release mode.
+		startFullscreen: false 			// Whether to start the game in fullscreen on desktop targets
+	};
+
 	public static final game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
@@ -62,14 +95,46 @@ class Main extends Sprite
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
+	public static var sprite:Main;
+	public static var game:FlxFunkGame;
+	#if !mobile
+	public static var fpsCounter:FPS_Mem; //The FPS display child
+	#end
+	#if DEV_TOOLS
+	public static var console:ScriptConsole;
+	#end
+	public static var transition:Transition;
+	public static var engineVersion(default, never):String = "1.0.0-b.2"; //The engine version, if its not the same as the github one itll open OutdatedSubState
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+
+
+
+	#if desktop
+	static function errorMsg(error:Dynamic)
+	{
+		Application.current.window.alert(Std.string(error is UncaughtErrorEvent ? error.error : error), "Uncaught Error");
+		DiscordClient.shutdown();
+		Sys.exit(1);
+	}
+	#end
+
 	public static function main():Void
 	{
-		Lib.current.addChild(new Main());
+		Lib.current.addChild(sprite = new Main());
+		#if desktop
+			Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, errorMsg);
+			#if cpp	
+				untyped __global__.__hxcpp_set_critical_error_handler(errorMsg);
+			#end
+		#end	
 	}
 
 	public function new()
 	{
-		super();
+		super();	
+		stage != null ? init() : addEventListener(Event.ADDED_TO_STAGE, init);
+
 
 		#if (cpp && windows)
 		backend.Native.fixScaling();
@@ -202,11 +267,64 @@ class Main extends Sprite
 		});
 	}
 
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+
+		moonchart.backend.FormatDetector.defaultFileFormatter = (title, diff) -> {
+			return [diff.trim().toLowerCase()];
+		}
+
+		();
+	}
+
+	public static var DEFAULT_GRAPHIC(default, null):GlobalGraphic = null;
+
 	static function resetSpriteCache(sprite:Sprite):Void {
 		@:privateAccess {
 		        sprite.__cacheBitmap = null;
 			sprite.__cacheBitmapData = null;
 		}
+	}
+
+	private function setupGame():Void {
+		final stageWidth:Int = Lib.current.stage.stageWidth;
+		final stageHeight:Int = Lib.current.stage.stageHeight;
+		
+		@:privateAccess
+		DEFAULT_GRAPHIC = new GlobalGraphic(null, FlxAssets.getBitmapData("flixel/images/logo/default.png"));
+
+		if (settings.zoom == -1.0) {
+			final ratioX:Float = stageWidth / settings.width;
+			final ratioY:Float = stageHeight / settings.height;
+			settings.zoom = Math.min(ratioX, ratioY);
+			settings.width = Math.ceil(stageWidth / settings.zoom);
+			settings.height = Math.ceil(stageHeight / settings.zoom);
+		}
+
+		addChild(game = new FlxFunkGame(settings.width, settings.height, settings.initialState, settings.framerate, settings.framerate, settings.skipSplash, settings.startFullscreen));
+	}
+
+	public static function resizeGame(resolution:String) {
+		#if desktop
+		var resize = function (w:Int, h:Int) {
+			FlxG.resizeWindow(w, h);
+			Lib.application.window.x = Std.int((FlxG.stage.fullScreenWidth - w) * 0.5);
+			Lib.application.window.y = Std.int((FlxG.stage.fullScreenHeight - h) * 0.5);
+		}
+		
+		switch (resolution) {
+			case "256x144": resize(256, 144);
+			case "640x360": resize(640, 360);
+			case "854x480": resize(854, 480);
+			case "960x540": resize(960, 540);
+			case "1024x576": resize(1024, 576);
+			case "1280x720": resize(1280, 720);
+			case "native": resize(FlxG.stage.fullScreenWidth, FlxG.stage.fullScreenHeight);
+			default: resize(FlxG.initialWidth, FlxG.initialHeight);
+		}
+		#end
 	}
 
 	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
@@ -258,4 +376,5 @@ class Main extends Sprite
 		Sys.exit(1);
 	}
 	#end
+    }
 }
