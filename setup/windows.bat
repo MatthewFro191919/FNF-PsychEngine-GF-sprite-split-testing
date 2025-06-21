7d48790b3e68334c7e3e69de69401e29b93a3b39
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-21 12:37:20 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,8 +4,6 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
@@ -13,8 +11,10 @@ haxelib install tjson 1.4.0
 haxelib install hscript-iris 1.1.3
 haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.0.1 --skip-dependencies
-haxelib set lime 8.1.2
-haxelib set openfl 9.3.3
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib git moonchart https://github.com/MaybeMaru/moonchart
+haxelib git maru-hscript https://github.com/MaybeMaru/hscript-improved
 haxelib git flixel-ui https://github.com/HaxeFlixel/flixel-ui dev
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
